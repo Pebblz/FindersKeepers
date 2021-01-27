1d8d93d95f09272bb3fe5e62190dd2142be91998
Brandon <Blabrake1@gmail.com>
2021-01-27 18:04:25 -0500

    comments because pat needs them

--- Assets/Scripts/PickUpAbles.cs
+++ Assets/Scripts/PickUpAbles.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PickUpAbles : MonoBehaviour
 {
+    //an [] of Player objs
     GameObject[] Player = new GameObject[4];
+    //this is an awake because it'll do this whenever this object gets spawned
     void Awake()
     {
+        //finds the players
         Player = GameObject.FindGameObjectsWithTag("Player");
     }
 
@@ -15,10 +18,13 @@ public class PickUpAbles : MonoBehaviour
         
         for(int i = 0; i < Player.Length; i++)
         {
+            //checks how close the players are to the obj 
             if(Vector3.Distance(this.gameObject.transform.position,Player[i].transform.position) < 5)
             {
+                //if he's getting ready to pick up the obj
                 if(Player[i].GetComponent<Player>().isHoldingOBJ == false && Player[i].GetComponent<Player>().isPickingUpOBJ == true)
                 {
+                    //does this stuff
                     transform.parent = Player[i].transform;
                     transform.position = Player[i].transform.position + new Vector3(0, 1, 0);
                     Player[i].GetComponent<Player>().SetPickUpOBJ(this.gameObject);
