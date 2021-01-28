ba43f1578e4714d07263434afbac81c097ecb282
pnaatz <41494483+pnaatz@users.noreply.github.com>
2021-01-27 22:27:04 -0500

    Commented out the AI

--- Assets/Scripts/Steely_AI.cs
+++ Assets/Scripts/Steely_AI.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Steely_AI : MonoBehaviour
 {
 
+    /* Flower Box
+     * Programmer: Pat Naatz
+     * Bare minimal AI that picks things up and swaps them with other objects
+     * 
+     * ToDo
+     * Finish the random drop item function
+     * improve the AI direction choices
+     * Make AI increase speed when it picks up an item
+     * */
+
     Rigidbody rb;
     int speed = 1;
 
+    //Current Direction
     int H = 0;
     int V = 0;
 
+    //Pocket Variables
     GameObject holdingSomething = null;
-
     GameObject redList = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,12 +48,14 @@ public class Steely_AI : MonoBehaviour
     {
         while (true)
         {
+            //change directions
             H = Random.Range(-1, 2);
             V = Random.Range(-1, 2);
 
-            yield return new WaitForSeconds(Random.Range(1, 10));
+            yield return new WaitForSeconds(Random.Range(1, 10)); //wait for some time between 1 and 10 seconds
         }
     }
+
     //IEnumerator DropObject()
     //{Debug.Log("Dropped");
     //    while(holdingSomething != null)
@@ -54,11 +68,9 @@ public class Steely_AI : MonoBehaviour
     //}
 
     private void OnTriggerEnter(Collider other)
-    {
-       // Debug.Log("triggered");
+    {      //dont move walls and stuff         dont move the same thing twice
         if(other.tag == "Moveable" && other.gameObject != redList)
         {
-            //Debug.Log("internal log");
             PickUp(other.gameObject);
         }
     }
@@ -67,20 +79,24 @@ public class Steely_AI : MonoBehaviour
     {
         
         if (holdingSomething != null)
-        {
+        { //if holding something
             PutObjectHere(gameObject.transform.position);
             DirectionChange();
         }
+
+        //pick up the gameobject
         gameObject.transform.parent = transform;
-        gameObject.transform.position = transform.position + new Vector3(0, 1, 0);
+        gameObject.transform.position = transform.position + new Vector3(0, 1, 0); //todo make position dynamic
         holdingSomething = gameObject;
     }
 
     void PutObjectHere(Vector3 posistion)
     {
-        
+        //put down item
         holdingSomething.transform.position = posistion;
         holdingSomething.transform.parent = null;
+
+        //set up so the AI doesn't instantly pick it back up
         redList = holdingSomething;
     }
 }
