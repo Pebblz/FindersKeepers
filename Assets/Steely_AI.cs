fe7d4288c898f67b32b647c03e7fa34ea9815bea
pnaatz <41494483+pnaatz@users.noreply.github.com>
2021-01-27 22:16:30 -0500

    Demo AI
    Currently picks things up and swaps them with other things.

---
+++ Assets/Steely_AI.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steely_AI : MonoBehaviour
+{
+
+    Rigidbody rb;
+    int speed = 1;
+
+    int H = 0;
+    int V = 0;
+
+    GameObject holdingSomething = null;
+
+    GameObject redList = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine("DirectionChange");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Movement
+
+        Vector3 tempVect = new Vector3(H, 0, V);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + tempVect);
+        #endregion
+    }
+
+    IEnumerator DirectionChange()
+    {
+        while (true)
+        {
+            H = Random.Range(-1, 2);
+            V = Random.Range(-1, 2);
+
+            yield return new WaitForSeconds(Random.Range(1, 10));
+        }
+    }
+    //IEnumerator DropObject()
+    //{Debug.Log("Dropped");
+    //    while(holdingSomething != null)
+    //    {
+    //          yield return new WaitForSeconds(Random.Range(10, 60));
+    //        PutObjectHere(transform.position + new Vector3(holdingSomething.GetComponent<Renderer>().bounds.size.x * H * -1, 0, holdingSomething.GetComponent<Renderer>().bounds.size.x * V * -1));
+    //        holdingSomething = null;
+          
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // Debug.Log("triggered");
+        if(other.tag == "Moveable" && other.gameObject != redList)
+        {
+            //Debug.Log("internal log");
+            PickUp(other.gameObject);
+        }
+    }
+
+    void PickUp(GameObject gameObject)
+    {
+        
+        if (holdingSomething != null)
+        {
+            PutObjectHere(gameObject.transform.position);
+            DirectionChange();
+        }
+        gameObject.transform.parent = transform;
+        gameObject.transform.position = transform.position + new Vector3(0, 1, 0);
+        holdingSomething = gameObject;
+    }
+
+    void PutObjectHere(Vector3 posistion)
+    {
+        
+        holdingSomething.transform.position = posistion;
+        holdingSomething.transform.parent = null;
+        redList = holdingSomething;
+    }
+}
---
+++ Assets/Steelys_hit_box.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steelys_hit_box : MonoBehaviour
+{
+    Steely_AI steely;
+    // Start is called before the first frame update
+    void Start()
+    {
+        steely = GetComponentInParent<Steely_AI>();
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag != "Not Pick Upable")
+    //    {
+    //        steely.PickUp(other.gameObject);
+    //    }
+    //}
+}
