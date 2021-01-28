0e8e585c870251724695d7616d2e2858bf09a860
Josh Cormier <jcorm714@gmail.com>
2021-01-28 18:20:47 -0500

    Please work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -61,7 +61,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         }
 
         mainCam = GameObject.Find("Main Camera").GetComponent<Transform>();
-        //DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
         //DontDestroyOnLoad(mainCam);
 
     }
