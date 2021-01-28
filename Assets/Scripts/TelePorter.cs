696729655f85d9487a521104dce627a3747b9115
Brandon <Blabrake1@gmail.com>
2021-01-28 15:45:04 -0500

    TelePorter

---
+++ Assets/Scripts/TelePorter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelePorter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject OtherTelePorter;
+
+    public float TimeToTelePort;
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToTelePort -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            if (TimeToTelePort <= 0)
+            {
+                col.transform.position = OtherTelePorter.transform.position;
+                OtherTelePorter.GetComponent<TelePorter>().TimeToTelePort = 1;
+            }
+        }
+    }
+}
