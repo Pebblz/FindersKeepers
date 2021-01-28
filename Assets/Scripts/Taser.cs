e6864525ca15ec299ca739a380440492ed3d810f
Brandon <Blabrake1@gmail.com>
2021-01-27 20:39:43 -0500

    Place Holder Taser

---
+++ Assets/Scripts/Taser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taser : MonoBehaviour
+{
+    //this is so the player doesn't shoot himself 
+    public GameObject PlayerWhoShotThis;
+
+    float DestroyTimer = .7f;
+    void Update()
+    {
+        DestroyTimer -= Time.deltaTime;
+        if (DestroyTimer <= 0)
+            Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player" && col.gameObject != PlayerWhoShotThis)
+        {
+            col.GetComponent<Player>().StunPlayer();
+            Destroy(gameObject);
+        }
+        
+    }
+}
