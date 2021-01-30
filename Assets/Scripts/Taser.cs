55d40f35aaadbf74db5d106d0f9d409808b9050e
Brandon <Blabrake1@gmail.com>
2021-01-29 20:01:24 -0500

    Update Taser.cs

--- Assets/Scripts/Taser.cs
+++ Assets/Scripts/Taser.cs
@@ -17,22 +17,32 @@ public class Taser : MonoBehaviourPunCallbacks
         DestroyTimer -= Time.deltaTime;
         if (DestroyTimer <= 0)
         {
-            if (PhotonNetwork.IsMasterClient) {
+            if (PhotonNetwork.IsMasterClient)
+            {
                 PhotonNetwork.Destroy(this.gameObject);
             }
         }
     }
     private void OnTriggerEnter(Collider col)
     {
-        if(col.tag == "Player" && col.gameObject != PlayerWhoShotThis)
+
+        if (col.tag == "Player" && col.gameObject != PlayerWhoShotThis)
         {
             col.GetComponent<Player>().StunPlayer();
             col.GetComponent<PlayerPickUp>().DropOBJ();
-            if (PhotonNetwork.IsMasterClient)
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                photonView.RPC("DestroyOBJ", RpcTarget.All);
+            } else
             {
                 PhotonNetwork.Destroy(this.gameObject);
             }
         }
-        
+
+    }
+
+    public void DestroyOBJ()
+    {
+        PhotonNetwork.Destroy(this.gameObject);
     }
 }
