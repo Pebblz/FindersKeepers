896f705b345e102c1ca50044636963186dd78958
Jimmy <jmcgurn@email.neit.edu>
2021-01-29 21:57:26 -0500

    a

--- Assets/Scripts/SpeedPowerUpCollectible.cs
+++ Assets/Scripts/SpeedPowerUpCollectible.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 using Photon.Pun;
 
 
-public class SpeedPowerUpCollectible : MonoBehaviour
+public class SpeedPowerUpCollectible : MonoBehaviourPunCallbacks, IPunObservable
 {
     public PowerUp powerUp;
     public AudioClip pickUpNoise;
+    public bool collided = false;
 
     public void Awake()
     {
         powerUp = new SpeedPowerUp();
     }
+
+    public void Update()
+    {
+
+        //if (photonView.IsMine)
+        //{
+        //    if (collided)
+        //    {
+        //        PhotonNetwork.Destroy(this.gameObject);
+        //    }
+        //}
+        //else
+        //{
+        //    photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
+        //    if (collided)
+        //    {
+        //        PhotonNetwork.Destroy(this.gameObject);
+        //    }
+        //}
+    }
     public void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "Player")
+        if (!photonView.IsMine)
+        {
+            Debug.Log("o");
+            return;
+        }
+
+        if (collision.collider.tag == "Player" && !collided)
         {
             if (collision.collider.gameObject.GetComponent<Player>().getPowerUp() != null)
             {
@@ -23,8 +50,33 @@ public class SpeedPowerUpCollectible : MonoBehaviour
             }
             collision.collider.gameObject.GetComponent<Player>().setPowerUp(powerUp);
             collision.collider.gameObject.GetComponent<Player>().activatePowerUp();
-            //Destroy(this.gameObject);
-            PhotonNetwork.Destroy(this.gameObject);
-        }
+
+            if(photonView.IsMine)
+            {
+                Destroy(gameObject);
+                gameObject.GetComponent<PhotonView>().TransferOwnership(this.gameObject.GetComponent<PhotonView>().Owner);
+                gameObject.GetComponent<PhotonView>().RPC("DestroyGlobally", RpcTarget.All);
+            }          
+           // collided = true;
+        }        
+    }
+
+    [PunRPC]
+
+    public void DestroyGlobally()
+    {
+        PhotonNetwork.Destroy(gameObject);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        //if(stream.IsWriting)
+        //{
+        //    stream.SendNext(collided);
+        //}
+        //else
+        //{
+        //    collided = (bool)stream.ReceiveNext();
+        //}
     }
 }
