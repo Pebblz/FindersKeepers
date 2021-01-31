ae1424c89493cbafab757f3e7b03db1258b4ca64
Brandon <Blabrake1@gmail.com>
2021-01-30 20:02:55 -0500

    Update PlayerPickUp.cs

--- Assets/Scripts/PlayerPickUp.cs
+++ Assets/Scripts/PlayerPickUp.cs
@@ -42,6 +42,7 @@ public class PlayerPickUp : MonoBehaviourPunCallbacks, IPunObservable
     {
         scene = SceneManager.GetActiveScene();
         pickUpTimer -= Time.deltaTime;
+
         if (sceneID != lastIndex)
         {
             isPickingUpOBJ = false;
