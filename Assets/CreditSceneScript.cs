84ad396c6e2d9bf03d1a3e881bba2c3cab2ba212
pnaatz <41494483+pnaatz@users.noreply.github.com>
2021-01-31 10:46:51 -0500

    Credit Scene

---
+++ Assets/CreditSceneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditSceneScript : MonoBehaviour
+{
+    #region Button Function
+    public void Back()
+    {
+        SceneManager.LoadScene("Launcher");
+    }
+    #endregion
+}
