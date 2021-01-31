d75530088dc8b3d5bd536b9670e2b73af15b6e23
Josh Cormier <jcorm714@gmail.com>
2021-01-31 03:50:33 -0500

    sound stuff

---
+++ Assets/Scripts/SoundtrackManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackManager : MonoBehaviour
+{
+    [SerializeField] AudioSource soundtrack;
+    
+    public void switchAudioTracks()
+    {
+        if(soundtrack == null)
+        {
+            Debug.LogWarning("No secondary soundtrack provided");
+        }
+
+        GameObject camera = GameObject.Find("Main Camera");
+        AudioSource original = camera.GetComponentInChildren<AudioSource>();
+        original.Pause();
+        this.soundtrack.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    public void resumeOriginalTrack()
+    {
+
+        this.soundtrack.Pause();
+        GameObject camera = GameObject.Find("Main Camera");
+        AudioSource original = camera.GetComponentInChildren<AudioSource>();
+        original.Play();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            switchAudioTracks();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            resumeOriginalTrack();
+        }
+    }
+}
