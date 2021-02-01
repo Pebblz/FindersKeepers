fa7d149496161c71bf1f0b74c3cfce639999918e
Brandon <Blabrake1@gmail.com>
2021-01-31 22:04:14 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,9 +30,11 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     static GameState gameState = GameState.The_Run;
 
-    [SerializeField] Transform[] RespawnPoints;
-    [SerializeField] TodoList list;
-   // [SerializeField]
+    [SerializeField]
+    Transform[] RespawnPoints;
+    [SerializeField]
+    TodoList list;
+    // [SerializeField]
     //private GameObject speedPowerPrefab;
 
     public static GameObject[] Randomize(IEnumerable<GameObject> source)
@@ -45,10 +47,10 @@ public class GameManager : MonoBehaviourPunCallbacks
     void Start()
     {
         // do some linq stuff to order the players
-        var playerfabs  = Resources.LoadAll<GameObject>("Players");
+        var playerfabs = Resources.LoadAll<GameObject>("Players");
         var temp = from s in playerfabs
-                        orderby s.name descending
-                        select s;
+                   orderby s.name descending
+                   select s;
         playerPrefabs = temp.ToArray();
         playerPrefabs = GameManager.Randomize(playerPrefabs);
 
@@ -60,19 +62,19 @@ public class GameManager : MonoBehaviourPunCallbacks
         }
         else if (Player.localInstance == null)
         {
-                Debug.Log("Adding player #" + PhotonNetwork.CurrentRoom.PlayerCount);
-                
-                
-                int thatFuckingIdx = (PhotonNetwork.CurrentRoom.PlayerCount > 1)? FindFirstNotUsedSkin() : 0;
-                Debug.Log("Loading player skin: " + this.playerPrefabs[thatFuckingIdx].name);
+            Debug.Log("Adding player #" + PhotonNetwork.CurrentRoom.PlayerCount);
+
+
+            int thatFuckingIdx = (PhotonNetwork.CurrentRoom.PlayerCount > 1) ? FindFirstNotUsedSkin() : 0;
+            Debug.Log("Loading player skin: " + this.playerPrefabs[thatFuckingIdx].name);
 
 
             PhotonNetwork.Instantiate("Players/" + this.playerPrefabs[thatFuckingIdx].name, new Vector3(0f, 1f, 0f), Quaternion.identity);
 
             var props = new ExitGames.Client.Photon.Hashtable();
-                props.Add("skin", this.playerPrefabs[thatFuckingIdx].name);
-                PhotonNetwork.PlayerList[PhotonNetwork.CurrentRoom.PlayerCount -1].SetCustomProperties(props);
-        
+            props.Add("skin", this.playerPrefabs[thatFuckingIdx].name);
+            PhotonNetwork.PlayerList[PhotonNetwork.CurrentRoom.PlayerCount - 1].SetCustomProperties(props);
+
         }
         else
         {
@@ -93,31 +95,50 @@ public class GameManager : MonoBehaviourPunCallbacks
             }
         }
     }
-
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            //Finds current scene
+            Scene scene = SceneManager.GetActiveScene();
+            if (scene.name == "Main Game")
+            {
+                //if you're in game it'll lock your cursor and hide it 
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                //if you're not in game it'll unlock your cursor and make it visable
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
     public int FindFirstNotUsedSkin()
     {
-        
+
         List<string> allPlayerTypes = new List<string>();
-        foreach(var p in this.playerPrefabs)
+        foreach (var p in this.playerPrefabs)
         {
             allPlayerTypes.Add(p.name);
         }
 
         List<string> activePlayerTypes = getActivePlayerTypes();
         string str = "";
-        foreach(string s in activePlayerTypes)
+        foreach (string s in activePlayerTypes)
         {
             str += s + ", ";
         }
         Debug.Log("Active Player Types: " + str);
-        if(activePlayerTypes.Count == 1)
+        if (activePlayerTypes.Count == 1)
         {
             return 0;
         }
         var firstItem = allPlayerTypes.Except(activePlayerTypes).ToArray()[0];
-        for(int i = 0; i < allPlayerTypes.Count; i++)
+        for (int i = 0; i < allPlayerTypes.Count; i++)
         {
-            if(allPlayerTypes[i] == firstItem)
+            if (allPlayerTypes[i] == firstItem)
             {
                 return i;
             }
@@ -128,7 +149,7 @@ public class GameManager : MonoBehaviourPunCallbacks
     public List<string> getActivePlayerTypes()
     {
         List<string> typesOfColors = new List<string>();
-        foreach(var player in PhotonNetwork.PlayerList)
+        foreach (var player in PhotonNetwork.PlayerList)
         {
             typesOfColors.Add((string)player.CustomProperties["skin"]);
         }
@@ -168,7 +189,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         }
         NetworkSceneChangedRaiseEvent();
         PhotonNetwork.LoadLevel("Lobby_" + PhotonNetwork.CurrentRoom.PlayerCount);
-       
+
     }
 
     public void LoadGame()
@@ -193,7 +214,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     #region Network Events
     // the flag for raising a Network Event
-    
+
     private void NetworkSceneChangedRaiseEvent()
     {
         object[] content = new object[] { };
@@ -224,11 +245,11 @@ public class GameManager : MonoBehaviourPunCallbacks
     {
         gameState++; //increment gamestate
 
-        
+
         if ((int)gameState > 3)
         { //if gameover
             gameState = GameState.The_Run; //static so this variable must be manually reset
-            foreach(Player player in FindObjectsOfType<Player>())
+            foreach (Player player in FindObjectsOfType<Player>())
             {
                 player.GetComponent<Player_Movement>().enabled = false;
                 player.freeLookCam.SetActive(false);
