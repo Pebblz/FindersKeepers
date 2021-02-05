01b7a4535ed5f93211e81d67e9139cc1e642d4b6
pnaatz <41494483+pnaatz@users.noreply.github.com>
2021-02-05 14:28:45 -0500

    Credit Scene
    Added Comments

--- Assets/Scripts/CreditSceneScript.cs
+++ Assets/Scripts/CreditSceneScript.cs
@@ -5,10 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class CreditSceneScript : MonoBehaviour
 {
+    /*Flower Box
+     * Programmer: Patrick Naatz
+     * Objective: Make a script that returns you from the credit scene to the main scene
+     */
+
+        /// <summary>
+        /// Loads the Main scene (Launcher)
+        /// </summary>
     #region Button Function
     public void Back()
     {
-        SceneManager.LoadScene("Launcher");
+        //attach this function to the back button
+
+        SceneManager.LoadScene("Launcher"); //loads main scene
     }
     #endregion
 }
