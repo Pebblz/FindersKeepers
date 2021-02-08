7a5462ab8ece63fc5829ad4c76b2fb404484012d
pnaatz <41494483+pnaatz@users.noreply.github.com>
2021-02-08 00:02:12 -0500

    added lots of sound

---
+++ Assets/Scripts/Button_Focus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button_Focus : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] AudioSource hover;
+    [SerializeField] AudioSource click;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        hover.Play();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        hover.Stop();
+    }
+}
