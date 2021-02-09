a0a1f1871a3a8f0d3246945b66fa89c92d8344da
Patrick Naatz <pnaatz@email.neit.edu>
2021-02-09 09:28:44 -0500

    Settings Scene
    Made Settings scene accessable through launcher scene

---
+++ Assets/Scripts/Button_Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Script : MonoBehaviour
+{
+    /*
+     * Added Settings button
+     */
+      
+    /// <summary>
+    /// Loads the settings scene
+    /// </summary>
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
