9d63e8aacc3337fb30064f070412984853a671ca
Brandon <Blabrake1@gmail.com>
2021-02-09 15:27:42 -0500

    play button

---
+++ Assets/Scripts/InGameUI/PlayButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class PlayButton : MonoBehaviourPunCallbacks
+{
+    void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            if (GetComponent<PhotonView>().IsMine)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
