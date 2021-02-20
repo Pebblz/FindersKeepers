df05f89ca62a9a266c609d5a353a753de9acb1e0
Brandon <Blabrake1@gmail.com>
2021-02-20 01:17:11 -0500

    MiniMap and enter to text fields

---
+++ Assets/Scripts/MiniMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    GameObject Player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        transform.position = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z);
+
+    }
+}
