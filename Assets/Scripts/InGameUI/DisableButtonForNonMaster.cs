9be913134e8e7732bca39c56236fc3f653bca53e
Brandon <Blabrake1@gmail.com>
2021-03-02 19:00:46 -0500

    play button disables for non master clients

---
+++ Assets/Scripts/InGameUI/DisableButtonForNonMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class DisableButtonForNonMaster : MonoBehaviourPunCallbacks
+{
+
+    void Update()
+    {
+        if(!PhotonNetwork.IsMasterClient)
+        {
+            gameObject.SetActive(false);
+        } else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
