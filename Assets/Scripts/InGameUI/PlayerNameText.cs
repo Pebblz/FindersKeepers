143fc4ba15aa3586554de6e0bf4dc6820dfe7bdf
Brandon <Blabrake1@gmail.com>
2021-03-05 12:48:01 -0500

    nickname

--- Assets/Scripts/InGameUI/PlayerNameText.cs
+++ Assets/Scripts/InGameUI/PlayerNameText.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using Photon.Pun;
 namespace com.pebblz.finderskeepers
 {
-    public class PlayerNameText : MonoBehaviour
+    public class PlayerNameText : MonoBehaviourPunCallbacks
     {
         private TextMesh Tm;
         private GameObject Player;
@@ -13,30 +13,34 @@ namespace com.pebblz.finderskeepers
 
         void Start()
         {
-            camera = GameObject.FindGameObjectWithTag("MainCamera");
             Tm = GetComponent<TextMesh>();
-            Tm.text = GetComponentInParent<PhotonView>().Owner.NickName;
+            Tm.text = "";
         }
         void Update()
         {
-            //it makes sure the that if the player is the parent to not find the parent
-            //just so you don't constently see the name above your head but
-            //but the other players see your name
-            if (transform.parent.gameObject.tag != "Player")
+            if (!photonView.IsMine)
             {
-                if (Player == null)
+                if (camera == null)
                 {
-                    Player = GameObject.FindGameObjectWithTag("Player");
+                    camera = GameObject.FindGameObjectWithTag("MainCamera");
                 }
-            }else
-            {
-                Tm.text = "";
-            }
-            if (Player != null)
-            {
-                Vector3 temp = camera.gameObject.transform.forward;
-                temp.y = 90;
-                transform.rotation = Quaternion.LookRotation(-temp);
+                //it makes sure the that if the player is the parent to not find the parent
+                //just so you don't constently see the name above your head but
+                //but the other players see your name
+                if (transform.parent.gameObject.tag != "Player")
+                {
+                    if (Player == null)
+                    {
+                        Tm.text = GetComponentInParent<PhotonView>().Owner.NickName;
+                        Player = GameObject.FindGameObjectWithTag("Player");
+                    }
+                }
+                if (Player != null)
+                {
+                    Vector3 temp = camera.gameObject.transform.forward;
+                    temp.y = 90;
+                    transform.rotation = Quaternion.LookRotation(-temp);
+                }
             }
         }
     }
